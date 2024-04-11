2210151aa340ec0a754532ac297d2a13f9120931
Ranjit Sutar <ranjitsutar>
2024-04-12 00:49:35 +0530

    Example of Mono just and error method

--- src/main/java/com/reactivecoding/ReactiveCodingApplication.java
+++ src/main/java/com/reactivecoding/ReactiveCodingApplication.java
@@ -8,6 +8,8 @@ public class ReactiveCodingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReactiveCodingApplication.class, args);
-	}
 
+		System.out.println("Welcome");
+
+	}
 }
--- src/test/java/com/reactivecoding/ReactiveCodingApplicationTests.java
+++ src/test/java/com/reactivecoding/ReactiveCodingApplicationTests.java
@@ -2,6 +2,7 @@ package com.reactivecoding;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Mono;
 
 @SpringBootTest
 class ReactiveCodingApplicationTests {
@@ -9,5 +10,28 @@ class ReactiveCodingApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+	void workingWithMono(){
+//		Mono -> publisher that have 0-1 item
+		// create mono
+
+		Mono<String> errorMono = Mono.error(new RuntimeException("Error...."));
+
+		Mono<String> m1 = Mono.just("Ranjit")
+				.log()
+				.then(errorMono);
+
+		// consume mono
+//		m1.subscribe(data ->
+//						System.out.println("data is : "+data)
+//				);
+
+//		by using method referance
+		m1.subscribe(System.out::println);
+
+
+	}
+
 
 }
