75a9f5f9b2cab48bb1ba963615af689550b5022c
Ranjit Sutar <ranjitsutar>
2024-04-11 23:44:28 +0530

    setup project

---
+++ src/main/java/com/reactivecoding/ReactiveCodingApplication.java
@@ -0,0 +1,13 @@
+package com.reactivecoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveCodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveCodingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reactivecoding/ReactiveCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactivecoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveCodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
